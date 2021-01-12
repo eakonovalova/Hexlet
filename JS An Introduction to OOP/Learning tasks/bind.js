c3b5226d3c44eeafe26b2f19a07852aa55b66dab
e.a.konovalova <e.a.konovalova@tinkoff.ru>
2021-01-12 08:51:17 +0300

    add findFilesByName.js

--- JS An Introduction to OOP/Learning tasks/bind.js
+++ JS An Introduction to OOP/Learning tasks/bind.js
@@ -19,76 +19,7 @@
 // @ts-check
 
 // BEGIN (write your solution here)
-const bind = (obj, fn) => {
-    const newFn = (...args) => {
-        return fn.apply(obj, args)
-    };
-    return newFn;
-};
-export default bind;
-// END
-
-// @ts-check
-
-// BEGIN (write your solution here)
-const bind = (obj, fn) => {
-    const newFn = (...args) => fn.apply(obj, args)}
-return newFn;
-
-export default bind;
-// END
-
-// @ts-check
-
-// BEGIN (write your solution here)
-const bind = (obj, fn) => {
-    return (...args) => fn.apply(obj, args)
-};
-
-export default bind;
-// END
-
-
-// @ts-check
-
-// BEGIN (write your solution here)
 const bind = (obj, fn) => (...args) => fn.apply(obj, args);
 
 export default bind;
-// END
-
-
-// BEGIN
-export default (context, fn) => (...args) => fn.apply(context, args);
-
-/*
-Если разложить решение учителя на обычные функции, то получится такой код:
-const bind = function (context, fn) {
-  return function (...args) { // упаковка входных данных в массив
-    return fn.apply(context, args);
-  };
-};
-
-----
-
-Как это работает:
-
-bind обычно вызывается как метод на объекте функции,
-поэтому требуется передача аргументом только контекста:
-const obj = { number: 5 };
-const fn = function fn(number) {
-  return number + this.number;
-};
-const bindedFn1 = fn.bind(obj);
-в данной задаче bind является отдельной функцией, а не методом, поэтому fn передаётся аргументом:
-const bindedFn2 = bind(obj, fn); // возврат функции из функции
-
-bindedFn1(7); // 12
-bindedFn2(7); // 12
-
-----
-
-Альтернативное решение:
-const bind = (context, fn) => (...args) => fn.call(context, ...args);
-*/
 // END
